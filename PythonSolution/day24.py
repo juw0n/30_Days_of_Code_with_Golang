199d03151a8c38de04c13c107cb25aca16687427
juw0n <45376257+juw0n@users.noreply.github.com>
2020-08-27 00:00:48 +0100

    Add files via upload

---
+++ PythonSolution/day24.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def insert(self,head,data):
+            p = Node(data)           
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head  
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def removeDuplicates(self,head):
+        #Write your code here
+        current = head
+        newEntry = current.data
+        while current.next != None:
+            if current.next.data == newEntry:
+                current.next = current.next.next
+            else:
+                current = current.next
+                newEntry = current.data
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+head=mylist.removeDuplicates(head)
+mylist.display(head); 
